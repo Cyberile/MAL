8492ef8fef927d8dc9feacc216b63d2f0dc647d6
Moon Ape <kyosuke666@hotmail.com>
2022-05-10 18:38:18 +0800

    Update test.js

--- packages/client/static/js/test.js
+++ packages/client/static/js/test.js
@@ -1,9 +1,5 @@
-<html>
-   <body>
-      <div id="myDiv">Welcom to </div>
-   </body>
+
+    <div id="myDiv">Welcom to </div>
    <script type="text/javascript">
       var div = document.getElementById('myDiv');
       div.innerHTML += 'WayToLearnX!';
-   </script>
-</html>
