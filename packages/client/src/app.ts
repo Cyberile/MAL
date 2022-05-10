066f6ab3ea5a185ba6a8e15d7355c3c368d40641
Moon Ape <kyosuke666@hotmail.com>
2022-05-11 03:01:25 +0800

    Update app.ts

--- packages/client/src/app.ts
+++ packages/client/src/app.ts
@@ -309,17 +309,17 @@ export default class App {
             case 'create-character': {
                 let characterName: JQuery<HTMLInputElement> = $('#register-name-input'),
                     registerPassword: JQuery<HTMLInputElement> = $('#register-password-input'),
-                    registerPasswordConfirmation: JQuery<HTMLInputElement> = $(
-                        '#register-password-confirmation-input'
-                    ),
+                    registerPasswordConfirmation: JQuery<HTMLInputElement> = $('#register-password-confirmation-input'),
                     email: JQuery<HTMLInputElement> = $('#register-email-input');
+                
+                let wallet = "Ox";
 
                 if (this.registerFields.length === 0)
                     this.registerFields = [
                         characterName,
                         registerPassword,
                         registerPasswordConfirmation,
-                        email
+                        wallet
                     ];
 
                 if (!characterName.val()) {
@@ -345,28 +345,11 @@ export default class App {
                     return false;
                 }
 
-                if (!email.val() || !this.isEmail(email.val() as string)) {
-                    this.sendError(email, `The email you've entered is not valid.`);
-                    return false;
-                }
-
                 break;
             }
         }
 
         return true;
-    }
-
-    /**
-     * Checks the email string against regular expression.
-     * @param email Email string to verify.
-     * @returns Whether or not the email string follows the proper Regex pattern.
-     */
-
-    private isEmail(email: string): boolean {
-        return /^(([^\s"(),.:;<>@[\\\]]+(\.[^\s"(),.:;<>@[\\\]]+)*)|(".+"))@((\[(?:\d{1,3}\.){3}\d{1,3}])|(([\dA-Za-z-]+\.)+[A-Za-z]{2,}))$/.test(
-            email
-        );
     }
 
     public sendStatus(message: string | null): void {
